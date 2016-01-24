3ac6cf31b9bb632ef189dce55da907f417db57cb
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-01-24 15:07:29 +0100

    twitter configration

--- server/common/mongodb.js
+++ server/common/mongodb.js
@@ -3,5 +3,7 @@ import mongoose from "mongoose"
 mongoose.connect("mongodb://46.101.217.130/hackerstolz");
 
 export const TweetSchema = mongoose.Schema({
-	name: String
+	content: String,
+	date: { type: Date, default: Date.now },
+	cDate: { type: Date, default: Date.now }
 });
--- server/common/twitterapi.js
+++ server/common/twitterapi.js
@@ -1,11 +1,8 @@
 import Twitter from "twitter"
  
-var client = new Twitter({
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: ''
-});
+twitterConfig = require("./config.js");
+
+var client = new Twitter(twitterConfig);
 
 export const getTweets = (cb) => {
 	client.get('search/tweets', { q: 'node.js' }, (err, tweets, response) => {
